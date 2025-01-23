9cc49d9aa96cf4154b864afa097ce476f514ed41
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-01-23 09:44:41 -0700

    Get rid of explicitaves (it is school) and reduce file sizes

--- clock.js
+++ clock.js
@@ -9,15 +9,15 @@ var digitDotsEnabled = false;
 // Setup clock interval
 setInterval(function () {
   digitDotsEnabled = !digitDotsEnabled;
-  digitDots.src = "/images/clock/" + String(digitDotsEnabled) + ".png";
+  digitDots.src = "/images/clock/" + String(digitDotsEnabled) + ".gif";
 
   const now = new Date();
   digitHourTens.src =
-    "/images/clock/" + String(Math.floor(now.getHours() / 10)) + ".png";
+    "/images/clock/" + String(Math.floor(now.getHours() / 10)) + ".gif";
   digitHourOnes.src =
-    "/images/clock/" + String(Math.floor(now.getHours() % 10)) + ".png";
+    "/images/clock/" + String(Math.floor(now.getHours() % 10)) + ".gif";
   digitMinTens.src =
-    "/images/clock/" + String(Math.floor(now.getMinutes() / 10)) + ".png";
+    "/images/clock/" + String(Math.floor(now.getMinutes() / 10)) + ".gif";
   digitMinOnes.src =
-    "/images/clock/" + String(Math.floor(now.getMinutes() % 10)) + ".png";
+    "/images/clock/" + String(Math.floor(now.getMinutes() % 10)) + ".gif";
 }, 500);
